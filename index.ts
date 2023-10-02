1f5b599afacc68c252f4dd31607374b4a7113e96
Anurag Shivarathri <anurag6713@gmail.com>
2023-10-02 16:13:23 +0530

    Added logging

---
+++ index.ts
@@ -0,0 +1,62 @@
+/*
+ * Utility function to log to console based on log level
+ */
+
+const defaultConfig = {
+    emerg: true,
+    alert: true,
+    crit: true,
+    error: true,
+    warning: true,
+    notice: true,
+    info: true,
+    debug: true
+};
+
+type Config = typeof defaultConfig;
+
+/*
+ * Where we store the current config
+ */
+const currentConfig = {
+    ...defaultConfig,
+};
+
+/*
+ * The actual log function
+ * Eg: beemlog('error', 'Something went wrong');
+ */
+const beemlog = (
+    logLevel: keyof Config,
+    ...logs: any[]
+) => {
+    if (currentConfig[logLevel]) {
+        console.log(...logs);
+    }
+};
+
+/*
+ * Configure the log levels
+ */
+beemlog.config = function(newConfig: 'prod' | 'dev' | Partial<Config>) {
+    let newConfigObj: Partial<Config> = {};
+    if (newConfig === 'prod') {
+        newConfigObj = {
+            emerg: true,
+            alert: true,
+            crit: true,
+            error: true,
+            warning: true,
+            notice: true,
+        };
+    } else if (newConfig === 'dev') {
+        newConfigObj = defaultConfig;
+    } else {
+        newConfigObj = newConfig;
+    }
+    for (const logLevel in currentConfig) {
+        currentConfig[logLevel] = newConfig[logLevel] ?? false;
+    }
+}
+
+export default beemlog;
