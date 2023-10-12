06880940d96fea18c0881ea6820b9a5d7f3622f0
Anurag <anurag.shivarathri@trybeem.com>
2023-10-12 11:21:22 +0530

    Changed logging syntax

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 /*
- * Utility function to log to console based on log level
+ * Beemlog:
+ * utility function to log to console based on log level
  */
 
 const defaultConfig = {
@@ -24,21 +25,18 @@ const currentConfig = {
 
 /*
  * The actual log function
- * Eg: beemlog('error', 'Something went wrong');
+ * Eg: log('error', 'Something went wrong');
  */
-const beemlog = (
-    logLevel: keyof Config,
-    ...logs: any[]
-) => {
+function log(logLevel: keyof Config, ...logs: any[]) {
     if (currentConfig[logLevel]) {
-        console.log(`${logLevel}: `, ...logs);
+        console.log(`${logLevel}:`, ...logs);
     }
 };
 
 /*
  * Configure the log levels
  */
-beemlog.config = function(newConfig: 'prod' | 'dev' | Partial<Config>) {
+function config(newConfig: 'prod' | 'dev' | Partial<Config>) {
     let newConfigObj: Partial<Config> = {};
     if (newConfig === 'prod') {
         newConfigObj = {
@@ -55,8 +53,20 @@ beemlog.config = function(newConfig: 'prod' | 'dev' | Partial<Config>) {
         newConfigObj = newConfig;
     }
     for (const logLevel in currentConfig) {
-        currentConfig[logLevel] = newConfig[logLevel] ?? false;
+        currentConfig[logLevel] = newConfigObj[logLevel] ?? false;
     }
+}
+
+const beemlog = {
+    emerg: (...logs: any[]) => log('emerg', ...logs),
+    alert: (...logs: any[]) => log('alert', ...logs),
+    crit: (...logs: any[]) => log('crit', ...logs),
+    error: (...logs: any[]) => log('error', ...logs),
+    warning: (...logs: any[]) => log('warning', ...logs),
+    notice: (...logs: any[]) => log('notice', ...logs),
+    info: (...logs: any[]) => log('info', ...logs),
+    debug: (...logs: any[]) => log('debug', ...logs),
+    config,
 }
 
 export default beemlog;
